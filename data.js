f23f33a6dc9f04d8a4756b711198b68ea53dabda
IrwanNS <mirwanns2704@gmail.com>
2025-03-28 02:32:11 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+const nama = "Irwan N S" ;
+let usia 20 ;
+
+function generate()  {
+    if (usia === 20)
+// INI ADALAH KONDISI PERTAMA
+    console.log()
+} else{
+// INI ADALAH JIKA KONDISI TIDAK TERPENUHI
+console.log()
+}
+    
+
+}
